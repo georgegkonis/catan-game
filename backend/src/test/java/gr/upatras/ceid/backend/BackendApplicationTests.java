f947e37bd843e20adfae88988060e7bd4b7fe35d
George Gkonis <georgegkonis@outlook.com>
2023-05-07 21:58:05 +0300

    Add frontend

---
+++ backend/src/test/java/gr/upatras/ceid/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.upatras.ceid.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
